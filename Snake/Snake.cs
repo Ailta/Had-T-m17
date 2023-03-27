fe820dd85e6c0816b5f0129f4587b036d0c47210
Karel Kříž <KrizK21@spstrutnov.cz>
2023-03-27 10:33:01 +0200

    Hledá jídlo co je nejblíže a vyhne se překážkám včetně sám sebe

--- Snake/Snake.cs
+++ Snake/Snake.cs
@@ -1,4 +1,7 @@
-﻿namespace Snake
+﻿using System.Diagnostics;
+using System.Xml.Linq;
+
+namespace Snake
 {
     public partial class Snake
     {
@@ -23,18 +26,144 @@
             return string.Empty;
         }
 
+        private Direction SnakeMovement(Game game, int X, int Y)
+        {
+            Direction direction = Direction.Up;
+            var snake = game.You;
+
+            bool toLeft = true;
+            bool toRight = true;
+            bool toUp = true;
+            bool toDown = true;
+
+            for (int bodyI = 0; bodyI < snake.Body.Length; bodyI++)
+            {
+                var body = snake.Body[bodyI];
+
+                if (body.Y == snake.Head.Y)
+                {
+                    if (body.X == snake.Head.X - 1)
+                        toLeft = false;
+                    if (body.X == snake.Head.X + 1)
+                        toRight = false;
+                }
+
+                if (body.X == snake.Head.X)
+                {
+                    if (body.Y == snake.Head.Y - 1)
+                        toDown = false;
+                    if (body.Y == snake.Head.Y + 1)
+                        toUp = false;
+                }
+
+                //Debug.WriteLine($"{body.X}, {body.Y} ; {snake.Head.X}, {snake.Head.Y}; {closestFoodPos.X}, {closestFoodPos.Y}; {game.Board.Height}, {game.Board.Width}\nLeft: {toLeft}; Right: {toRight}\nUp: {toUp}; Down: {toDown}\n" +
+                //    $"----------------------");
+            }
+
+            for (int obstacleI = 0; obstacleI < game.Board.Obstacles.Length; obstacleI++)
+            {
+                var obstacle = game.Board.Obstacles[obstacleI];
+
+                if (obstacle.Y == snake.Head.Y)
+                {
+                    if (obstacle.X == snake.Head.X - 1)
+                        toLeft = false;
+                    if (obstacle.X == snake.Head.X + 1)
+                        toRight = false;
+                }
+
+                if (obstacle.X == snake.Head.X)
+                {
+                    if (obstacle.Y == snake.Head.Y - 1)
+                        toDown = false;
+                    if (obstacle.Y == snake.Head.Y + 1)
+                        toUp = false;
+                }
+            }
+
+            if (snake.Head.X - 1 < 0)
+                toLeft = false;
+            if (snake.Head.Y - 1 < 0)
+                toDown = false;
+            if (snake.Head.X + 1 >= game.Board.Width)
+                toRight = false;
+            if (snake.Head.Y + 1 >= game.Board.Height)
+                toUp = false;
+
+            Debug.WriteLine($"Left: {toLeft}; Right: {toRight}\nUp: {toUp}; Down: {toDown}");
+
+            if (snake.Head.X > X && toLeft)
+            {
+                direction = Direction.Left;
+                Debug.WriteLine("levá");
+            }
+            else if (snake.Head.X < X && toRight)
+            {
+                direction = Direction.Right;
+                Debug.WriteLine("pravá");
+            }
+            else if (snake.Head.Y < Y && toUp)
+            {
+                direction = Direction.Up; 
+                Debug.WriteLine("hore");
+            }
+            else if (snake.Head.Y > Y && toDown)
+            {
+                direction = Direction.Down;
+                Debug.WriteLine("dole");
+            }
+            else
+            {
+                if (toRight)
+                    direction = Direction.Right;
+                if (toLeft)
+                    direction = Direction.Left;
+                if (toUp)
+                    direction = Direction.Up;
+                if (toDown)
+                    direction = Direction.Down;
+            }
+
+            return direction;
+        }
+
         // dostane data hry, vraci smer pohybu hada
         public Direction Move(Game game)
         {
-            /*
-                SEM UMISTUJTE SVUJ KOD
-            */
+            Direction direction;
+            var snake = game.You;
 
-            // pro ukazku se vraci nahodny smer
-            Direction[] directions = (Direction[])Enum.GetValues(typeof(Direction));
-            Direction randomDirection = directions[(new Random()).Next(directions.Length)];
+            Coordinate closestFoodPos = new Coordinate();
+            closestFoodPos.X = 0;
+            closestFoodPos.Y = 0;
 
-            return randomDirection;
+            for (int foodI = 0; foodI < game.Board.Food.Length; foodI++)
+            {
+                var food = game.Board.Food[foodI];
+
+                int distanceXHeadToFood = snake.Head.X - food.X;
+                int distanceYHeadToFood = snake.Head.Y - food.Y;
+
+                int distanceXHeadToClosestFood = snake.Head.X - closestFoodPos.X;
+                int distanceYHeadToClosestFood = snake.Head.Y - closestFoodPos.Y;
+
+                double distanceHeadToFood = Math.Sqrt(distanceXHeadToFood * distanceXHeadToFood + distanceYHeadToFood * distanceYHeadToFood);
+                double closestDistanceHeadToFood = Math.Sqrt(distanceXHeadToClosestFood * distanceXHeadToClosestFood + distanceYHeadToClosestFood * distanceYHeadToClosestFood);
+
+                if (closestDistanceHeadToFood > distanceHeadToFood)
+                {
+                    closestFoodPos.X = food.X;
+                    closestFoodPos.Y = food.Y;
+                }
+            }
+
+
+            direction = SnakeMovement(game, closestFoodPos.X, closestFoodPos.Y);
+            /*if (snake.Head.X == closestFoodPos.X && snake.Head.Y == closestFoodPos.Y)
+                closestFoodPos = null;*/
+
+
+            return direction;
         }
     }
 }
